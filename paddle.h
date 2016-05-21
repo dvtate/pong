7a6b32ff3305ff70e00072f0034a7f13fcec23f5
Dustin Van Tate Testa <toast27@gmail.com>
2016-05-21 13:35:33 -0400

    good enough...

--- paddle.h
+++ paddle.h
@@ -1,26 +1,32 @@
 #ifndef PADDLE_H
 #define PADDLE_H
 
+#include <stdint.h>
+
+#include <SDL/SDL.h>
+
+
+extern uint8_t difficulty;
+extern const uint16_t screenHeight;
+
+
+
 class Paddle {
 public:
+	static const uint8_t paddleWidth = 20;
 	SDL_Rect rect;
-	uint16_t x, y;
-	Paddle(uint16_t _x, uint16_t _y):
-		x(_x), y(_y)
-	{
+	Paddle(const uint16_t& x, const uint16_t& y){
 		rect.x = x;
 		rect.y = y;
 		rect.h = 60;
-		rect.w = 20;
+		rect.w = paddleWidth;
 
 	}
-	Paddle(uint16_t _x, uint16_t _y, uint16_t paddleHeight):
-		x(_x), y(_y)
-	{
-		rect.x = x;
-		rect.y = y;
+	Paddle(const uint16_t& _x, const uint16_t& _y, const uint16_t& paddleHeight){
+		rect.x = _x;
+		rect.y = _y;
 		rect.h = paddleHeight;
-		rect.w = 20;
+		rect.w = paddleWidth;
 
 	}
 	~Paddle(){}
@@ -28,8 +34,37 @@ public:
 	SDL_Rect& getRect()
 		{ return rect; }
 
-	void translate(uint32_t
-};
 
+	void moveUpDown(const int16_t& yChange){
+
+		if (yChange == 0)
+			return;
+
+		if (yChange < 0) {
+			if (rect.y + yChange < 0)
+				return;
+			else
+				rect.y += yChange;
+			return;
+		}
+		if (rect.y + yChange + rect.h > screenHeight)
+			return;
+		else
+			rect.y += yChange;
+
+	}
+
+	void moveUpDown(bool up, bool down){
+
+		int8_t change = 0;
+
+		if (up)
+			change -= 1;
+		if (down)
+			change += 1;
+
+		moveUpDown(change);
+	}
+};
 
 #endif
